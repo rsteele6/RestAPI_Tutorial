206869024f1997c18c284218e5d0821f8b9026b7
rsteele6 <rsteele6@gmail.com>
2022-02-24 17:54:59 -0500

    added placeholder index page

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>REST API Tutorial</title>
+</head>
+<body>
+    <h1>Placeholder Page</h1>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur laboriosam et ea.</p>
+</body>
+</html>
+
+<?php
+
+?>
